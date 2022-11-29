067aedcf76f4f3d189d46d5569647de35063cfff
muhdsaeed1 <olayinkasaheed32@gmail.com>
2022-11-29 08:56:00 +0000

    if-else loops in py

---
+++ 102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Saheed Olayinka 
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
---
+++ 7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Saheed Olayinka
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+
